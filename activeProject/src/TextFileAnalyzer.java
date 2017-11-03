e458cc302911d03ddd78db54ce830ba39b295008
Austin Peterson <austintpeterson@gmail.com>
2017-11-03 13:03:09 -0700

    got the main structure going

--- activeProject/src/TextFileAnalyzer.java
+++ activeProject/src/TextFileAnalyzer.java
@@ -1,18 +1,45 @@
+import java.util.Vector;
 
 public class TextFileAnalyzer {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-		//this is the entry point of our whole program
 		
-		//call GUI
+		new GUI();//Zoe find a way to return the pathway/buttons people choose
+		//for starters, return an vector/whatever of pathways the user chooses.
+		//could be 10, could be 1.
 		
-		//objs
 		
-		System.out.println("Hello, World!");
 		
-		new GUI();
+		//placeholders/testers for returned values/data from GUI
+		Vector<String> pathways = new Vector<String>();//ZOE RETURN THIS DATA STRUCTURE
+		pathways.addElement("/Users/austinpeterson/Desktop/test.txt");//easy testing
+		
+		
+		
+		//FileIO for creating FileAttrib objects
+		FileIO myFileIO = new FileIO();
+		
+		//vector of FileAttribs that is the size of the pathways returned
+		Vector<FileAttribs> selectedFiles = new Vector<FileAttribs>();
+		
+		//iterate through provided pathways
+		for (int i = 0; i < pathways.size(); i++){
+			//call FileIO with current pathway
+			FileAttribs tempFile = myFileIO.FileRead(pathways.elementAt(i));
+			
+			//if pathway is invalid, skip this pathway
+			if(myFileIO.FileRead(pathways.elementAt(i)) == null){
+				System.out.println("opening File at: " + pathways.elementAt(i) + " [FAILED]");
+			}else{
+				selectedFiles.addElement(tempFile);
+				System.out.println("opening File at: " + pathways.elementAt(i) + " [SUCCESS]");
+			}
+			
+			
+		}
+		
+		
+		
 	}
 
 }
